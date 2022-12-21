cd3a02f89243040ae130c74c9c8f82c1d53ae85e
Larson <andlars@UMICH.EDU>
2022-12-21 16:56:16 -0500

    Tracking simple Abaqus simulations, textbooks, etc. First commit.

---
+++ Getting Started With Abaqus/code/meas_to_nodal_vals.py
@@ -0,0 +1,25 @@
+
+
+# Input:
+#    A .inp file defining an Abaqus simulation.
+# Functionality:
+#    Parses the portion of the .inp file which lists the node numbers and
+#       nodal coordinates.
+# Return:
+#    List of tuples. Each tuple contains: node number, x coord, y coord, z
+#       coord.
+def parse_nodes(file_name):
+
+
+# Input: 
+#    File containing nodes at absolute path.
+# Functionality: 
+#    Collects groups of nodes with the same y-coordinates.
+# Return:
+#    List of tuples. Each tuple contains a y-coordinate and a list of nodes
+#       which lie at that y-coordinate.
+def nodal_coords_to_groups():
+
+
+
+
