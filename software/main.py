b573d2aaee4de4ed7d65f60d818413b43f90d7b6
Andrew Larson <alarson2017@yahoo.com>
2023-07-03 12:36:21 -0400

    Gutentags is acting funny...

--- software/main.py
+++ software/main.py
@@ -3,13 +3,17 @@ from abaqus import *
 from abaqusConstants import *
 
 
-
-
 # TODO: Can this be made more elegant? 
 import sys
 sys.path.insert(0, "/home/andlars/Desktop/umich_machining_project/software/")
 
 
+
+
+def hello():
+    print("...")
+
+hello()
 
 
 
