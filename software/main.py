a19bdbf893d29b6cd7e99668f7e1d3b7b2e64947
Andrew Larson <alarson2017@yahoo.com>
2023-07-03 12:07:31 -0400

    Testing ctags generation via Git hook

--- software/main.py
+++ software/main.py
@@ -2,6 +2,9 @@
 from abaqus import *
 from abaqusConstants import *
 
+
+
+
 # TODO: Can this be made more elegant? 
 import sys
 sys.path.insert(0, "/home/andlars/Desktop/umich_machining_project/software/")
